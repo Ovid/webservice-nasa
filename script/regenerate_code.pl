71d7e3e90538d97a2b13ec752cf9927feaa5fc5e
Ovid <ovid@cpan.org>
2023-06-16 17:53:44 +0200

    Document our code regeneration script

--- script/regenerate_code.pl
+++ script/regenerate_code.pl
@@ -22,3 +22,29 @@ my $generator = WebService::NASA::Generate->new(
     verbose   => $verbose,
 );
 $generator->run;
+
+__END__
+
+=head1 NAME
+
+regenerate_code.pl - Regenerate the WebService::NASA code
+
+=head1 SYNOPSIS
+
+	regenerate_code.pl [options]
+
+	Options:
+
+	Option      Argument	Description
+	------      --------	-----------
+	--openapi   Filename    Path to the OpenAPI file to use (defaults to nasa/openapi.yaml)
+	--debug     -           Enable debugging output
+	--overwrite -           Overwrite existing files if 
+	--verbose   -           Enable verbose output
+	--nowrite   -           Don't write the files
+
+=head1 DESCRIPTION
+
+This code is used to regenerate the WebService::NASA code from the OpenAPI
+specification. Update F<nasa/openapi.yaml> and run this script to regenerate
+the code.
